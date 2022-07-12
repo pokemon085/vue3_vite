e72f9065918b81f2930a69cc692aa1e5ba0da985
tina <pokemon0850110@gmail.com>
2022-07-12 21:39:44 +0800

    計算屬性

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <componentA />
 </template>
-
+<script setup>
+import componentA from './components/componentA.vue';
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/componentA.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Computed 計算屬性</h1>
+  <input type="number" v-model="num">
+  <h3>{{ numFormat }}</h3>
+
+  <input type="text" v-model="fullName"><br/>
+  {{firstName}}<br/><br/>{{lastName}}
+</template>
+<script setup>
+import { computed,ref } from "vue";
+const num = ref(0);
+const firstName = ref('john');
+const lastName = ref('doe');
+
+//computed寫法1
+const numFormat = computed(() => {
+  if (Number.isNaN(num.value * 100)) return num.value;
+  return `${num.value * 100}%`;
+});
+
+//computed寫法2
+const fullName=computed({
+    get(){
+        return `${firstName.value}_${lastName.value}`
+    },
+    set(newName){
+        const [newFirstName,newlastName]=newName.split('_')
+        firstName.value=newFirstName
+        lastName.value=newlastName
+    }
+})
+</script>
+<style>
+</style>
